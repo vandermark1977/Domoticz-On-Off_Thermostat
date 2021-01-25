eabffd613c40c54cb5fcd8087653e231c8aee68c
vandermark1977 <49677940+vandermark1977@users.noreply.github.com>
2021-01-25 13:01:25 +0100

    Update Domoticz-On-Off_Thermostat.lua

--- Domoticz-On-Off_Thermostat.lua
+++ Domoticz-On-Off_Thermostat.lua
@@ -9,7 +9,7 @@ Description : This script is a simple on/off thermostat based on the roomtempera
 return {
    on = {
         timer = {
-                   'every 10 minutes'
+                   'every 5 minutes'
                 }
          },
 
@@ -24,31 +24,34 @@ return {
         local setPointId = 146          -- Dummy thermostaat device
         local roomTemperatureId = 42    -- Temperature measurement
         local wpSwitchId = 60           -- Heatpump_State
+        local DefrostSwitchId = 81      -- Defrost_State: Needed to prevent SwitchOff during a Defrost
         local setPoint = domoticz.utils.round(domoticz.devices(setPointId).setPoint, 2)
 
         -- script default values settings
         local roomTemperature = tonumber(domoticz.devices(roomTemperatureId).rawData[1])
 
-
-        if ((roomTemperature > (setPoint + 0.2)) and (domoticz.devices(wpSwitchId).state == 'On')) then
+        if ((roomTemperature > (setPoint + 0.2)) and (domoticz.devices(wpSwitchId).state == 'On') and (domoticz.devices(DefrostSwitchId).state == 'Off')) then
             if (true == switchWp) then
                 domoticz.devices(wpSwitchId).switchOff()
                 domoticz.notify('De warmtepomp is uitgezet door de thermostaat')
                 domoticz.log('WP UIT gezet: Temperatuur binnen is: '.. roomTemperature .. ' oC en doeltemperatuur is: '  .. setPoint .. ' oC ', domoticz.LOG_DEBUG)
             end
-        elseif ((roomTemperature < (setPoint - 0.2)) and (domoticz.devices(wpSwitchId).state == 'Off')) then
+        elseif ((roomTemperature < setPoint) and (domoticz.devices(wpSwitchId).state == 'Off')) then
             if (true == switchWp) then
                 domoticz.devices(wpSwitchId).switchOn()
                 domoticz.notify('De warmtepomp is aangezet door de thermostaat')
                 domoticz.log('WP AAN gezet: Temperatuur binnen is: '.. roomTemperature .. ' oC en doeltemperatuur is: '  .. setPoint .. ' oC ', domoticz.LOG_DEBUG)
             end
-        elseif (roomTemperature > (setPoint - 0.2)) and (roomTemperature < (setPoint + 0.2)) then
+        elseif ((roomTemperature > (setPoint + 1)) or (roomTemperature < (setPoint - 1.0))) and (domoticz.devices(wpSwitchId).state == 'On') then
              if (true == switchWp) then
-                domoticz.log('WP niet veranderd: Temperatuur binnen is: '.. roomTemperature .. ' oC en doeltemperatuur is: '  .. setPoint .. ' oC ', domoticz.LOG_DEBUG)
+                domoticz.notify('Check WP die aan staat! Temperatuur binnen is: '.. roomTemperature .. ' oC en doeltemperatuur is: '  .. setPoint .. ' oC ')
+                domoticz.log('Check WP die aan staat! Temperatuur binnen is: '.. roomTemperature .. ' oC en doeltemperatuur is: '  .. setPoint .. ' oC ', domoticz.LOG_DEBUG)
+            end 
+        elseif (roomTemperature > (setPoint - 1.0)) and (roomTemperature < (setPoint + 1.0) and (domoticz.devices(wpSwitchId).state == 'On')) then
+             if (true == switchWp) then
+                domoticz.log('WP AAN en niet veranderd: Temperatuur binnen is: '.. roomTemperature .. ' oC en doeltemperatuur is: '  .. setPoint .. ' oC ', domoticz.LOG_DEBUG)
             end    
         end
-
         domoticz.devices(setPointId).updateSetPoint(setPoint) -- update dummy sensor in case of red indicator ;-)
-
     end
 }
